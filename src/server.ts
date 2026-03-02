44d109293e0a30e582ecb08636907a59646a6064
Ryan Rosario <61715835+ryan-rosario@users.noreply.github.com>
2026-03-01 19:37:29 -0600

    Setup TypeScript Express backend with dev server

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+dotenv.config()
+
+const app = express();
+const PORT = process.env.port || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Habit Tracker Backend Running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
